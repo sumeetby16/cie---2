d5042bc639113df1a76b3aacf497665c742b96db
sumeetby16 <sumeetby16>
2025-10-30 15:43:30 +0530

    teacher

--- std_tea.py
+++ std_tea.py
@@ -7,3 +7,11 @@ print(f"Student name: {name}")
 print(f"Student age: {age}")
 print(f"Student USN: {usn}")
 print(f"Student Address: {add}")
+
+name = input("Enter teacher name: ")
+sub = input("Enter teacher sub: ")
+cc = input("Enter teacher cource code: ")
+
+print(f"Student name: {name}")
+print(f"teacher subject: {sub}")
+print(f"teacher cource code: {cc}")
