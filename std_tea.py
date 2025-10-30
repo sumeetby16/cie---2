05b0e8813920159adfb39d975c109f2c51b56f6e
sumeetby16 <sumeetby16@gmail.com>
2025-10-30 15:37:51 +0530

    Add student information input and display

---
+++ std_tea.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+usn = input("Enter your USN: ")
+add = input("Enter your Address: ")
+
+print(f"Student name: {name}")
+print(f"Student age: {age}")
+print(f"Student USN: {usn}")
+print(f"Student Address: {add}")
